d8769a93c7d17fbf2dfaaddfb4268ef0b477d9eb
Kaawainsw <nasywaafajri14@gmail.com>
2026-05-25 21:08:40 +0700

    2_Booking.py

--- pages/2_Booking.py
+++ pages/2_Booking.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
+st.markdown("""
+<style>
+
+.stApp{
+    background: linear-gradient(
+        135deg,
+        #8b0000,
+        #fff8ee
+    );
+}
+</stylle>
+""", unsafe_allow_html=True)
 st.title("🎟 BOOKING TIKET")
 
 df = pd.read_csv("data.csv")
@@ -39,6 +51,82 @@ seat = st.selectbox(
     seat_tersedia
 )
 
+st.write("---")
+
+st.subheader("✏ UPDATE SEAT")
+
+pending2 = df[
+    df["Status"] == "PENDING"
+]
+
+if not pending2.empty:
+
+    seat_lama = st.selectbox(
+        "Seat Lama",
+        pending2["Seat"],
+        key="update"
+    )
+
+    seat_baru = st.text_input(
+        "Seat Baru"
+    )
+
+    if st.button("Update Seat"):
+
+        booked = df["Seat"].tolist()
+
+        if seat_baru in booked:
+
+            st.error("Seat sudah dipakai!")
+
+        else:
+
+            df.loc[
+                df["Seat"] == seat_lama,
+                "Seat"
+            ] = seat_baru
+
+            df.to_csv(
+                "data.csv",
+                index=False
+            )
+
+            st.success(
+                "Seat berhasil diupdate!"
+            )
+
+st.write("---")
+
+st.subheader("🗑 DELETE PESANAN")
+
+if not pending2.empty:
+
+    delete_seat = st.selectbox(
+        "Pilih Seat",
+        pending2["Seat"],
+        key="delete"
+    )
+
+    if st.button("Hapus Pesanan"):
+            
+        df = df[
+        df["Seat"] != delete_seat
+        ]
+
+        df.to_csv("data.csv",
+            index=False)
+
+        st.success("Pesanan berhasil dihapus!")
+
+metode = st.selectbox(
+    "Metode Pembayaran",
+    [
+        "Debit",
+        "Kredit",
+        "QRIS"
+    ]
+)
+
 harga = harga_tiket[kategori]
 
 pajak = harga * 0.10
@@ -55,6 +143,43 @@ st.write(f"Biaya Admin : Rp {admin:,}")
 
 st.success(f"TOTAL : Rp {total:,}")
 
+# =========================
+# E-TICKET
+# =========================
+
+kode_tiket = f"{seat}-{nama}"
+
+eticket = f"""
+========== E-TICKET ==========
+TXT WORLD TOUR
+
+Nama : {nama}
+Seat : {seat}
+Kategori : {kategori}
+
+Metode Pembayaran : {metode}
+
+Subtotal : Rp {harga:,}
+Pajak : Rp {pajak:,}
+Biaya Admin : Rp {admin:,}
+
+TOTAL : Rp {total:,}
+
+Status : PENDING
+
+Kode Tiket :
+{kode_tiket}
+
+==============================
+"""
+
+st.download_button(
+    label="🎫 Download E-Ticket",
+    data=eticket,
+    file_name=f"{kode_tiket}.txt",
+    mime="text/plain"
+)
+
 if st.button("Konfirmasi Pesanan"):
 
     data_baru = pd.DataFrame([{
@@ -65,6 +190,7 @@ if st.button("Konfirmasi Pesanan"):
         "Pajak":pajak,
         "Admin":admin,
         "Total":total,
+        "Metode":metode,
         "Status":"PENDING"
     }])
 
