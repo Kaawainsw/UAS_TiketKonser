8ad1680e06d669d5bdb0d55849f0216ac92094fa
Kaawainsw <nasywaafajri14@gmail.com>
2026-05-25 19:39:47 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TXT Concert Ticket",
+    layout="wide"
+)
+
+st.title("🎫 TXT CONCERT TICKET SYSTEM")
+
+st.write("""
+Selamat datang di sistem pemesanan tiket konser TXT 💙
+
+Gunakan menu di sidebar untuk:
+- Melihat seat konser
+- Booking tiket
+- Admin panel
+""")
